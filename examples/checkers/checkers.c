5dd8a8c570b98b9e23112fc1ed86c65a4ebe6b9a
LiquidLemon <LiquidLemon@users.noreply.github.com>
2016-05-12 20:13:14 +0200

    Made changes to the library

--- examples/checkers/checkers.c
+++ examples/checkers/checkers.c
@@ -1,9 +1,27 @@
+#include <stdio.h>
 #include "../../lib.c"
+
+char checkers[8][8] = {
+  {1, 0, 1, 0, 1, 0, 1, 0},
+  {0, 1, 0, 1, 0, 1, 0, 1},
+  {1, 0, 1, 0, 1, 0, 1, 0},
+  {0, 1, 0, 1, 0, 1, 0, 1},
+  {1, 0, 1, 0, 1, 0, 1, 0},
+  {0, 1, 0, 1, 0, 1, 0, 1},
+  {1, 0, 1, 0, 1, 0, 1, 0},
+  {0, 1, 0, 1, 0, 1, 0, 1}
+};
+
+PI_THREAD (refresh_loop) {
+  while(1) { refresh(); }
+}
 
 int main() {
   init();
-
-  while (1) {
-    refresh();
+  setMap(checkers);
+  if (piThreadCreate(refresh_loop) != 0) {
+    puts("Thread didn't start");
+    return -1;
   }
+  while (1) {}
 }
--- lib.c
+++ lib.c
@@ -1,25 +1,54 @@
 #include <wiringPi.h>
 
-const int rows[] = {8, 9, 7, 15, 16, 0, 2, 3};
-const int columns[] = {4, 5, 12, 13, 6, 14, 10, 11};
+const char rows[] = {8, 9, 7, 15, 16, 0, 2, 3};
+const char columns[] = {4, 5, 12, 13, 6, 14, 10, 11};
 
-int displayMap[8][8] = {
-  {1, 0, 1, 0, 1, 0, 1, 0},
-  {0, 1, 0, 1, 0, 1, 0, 1},
-  {1, 0, 1, 0, 1, 0, 1, 0},
-  {0, 1, 0, 1, 0, 1, 0, 1},
-  {1, 0, 1, 0, 1, 0, 1, 0},
-  {0, 1, 0, 1, 0, 1, 0, 1},
-  {1, 0, 1, 0, 1, 0, 1, 0},
-  {0, 1, 0, 1, 0, 1, 0, 1}
+char displayBuffer[8][8] = {
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0}
 };
 
+// Broken, I'm not sure I need this so I'm just gonna leave it here
+//
+// char displayMap[8][8];
+//
+// void swapBuffer() {
+//   int tmp[8][8];
+//   tmp = displayBuffer;
+//   displayBuffer = displayMap;
+//   displayMap = tmp;
+//   
+//   int x, y;
+//   for (x = 0; x < 8; ++x) {
+//     for (y = 0; y < 8; ++y) {
+//       displayMap[x][y] = displayBuffer[x][y];
+//     }
+//   }
+// }
+
+void setMap(char map[8][8]) {
+  int x, y;
+  for (x = 0; x < 8; ++x) {
+    for (y = 0; y < 8; ++y) {
+      displayBuffer[x][y] = map[x][y];
+    }
+  }
+} 
+
+// Displays the contents of displayBuffer
+// Has to be calle continously to work
 void refresh() {
   int x, y;
   for (x = 0; x < 8; ++x) {
     digitalWrite(columns[x], LOW);
     for (y = 0; y < 8; ++y) {
-      if (displayMap[x][y]) {
+      if (displayBuffer[x][y]) {
         digitalWrite(rows[y], HIGH);
         digitalWrite(rows[y], LOW);
       }  
@@ -28,6 +57,53 @@ void refresh() {
   }
 }
 
+void changeLED(int x, int y, int state) {
+  displayBuffer[x][y] = state;
+}
+
+void setLED(int x, int y) {
+  changeLED(x, y, 1);
+}
+
+void clearLED(int x, int y) {
+  changeLED(x, y, 0);
+}
+
+void invertLED(int x, int y) {
+  if (displayBuffer[x][y])
+    displayBuffer[x][y] = 0;
+  else
+    displayBuffer[x][y] = 1;
+}
+
+void invertDisplay() {
+  int x, y;
+  for (x = 0; x < 8; ++x) {
+    for (y = 0; y < 8; ++y) {
+      invertLED(x, y);
+    }
+  }
+}
+
+void scrollDisplay() {
+  int i, j;
+  char tmp[8];
+  for (i = 0; i < 8; ++i) {
+    tmp[i] = displayBuffer[7][i];
+  }
+  
+  for (i = 7; i > 0; --i) {
+    for (j = 0; j < 8; ++j) {
+      displayBuffer[i][j] = displayBuffer[i-1][j];
+    }  
+  }
+
+  for (i = 0; i < 8; ++i) {
+    displayBuffer[0][i] = tmp[i];
+  }
+}
+
+// The following functions modify the display directly
 void setSingleLED(int x, int y) {
   int i;
   for (i = 0; i < 8; ++i) {
@@ -65,7 +141,8 @@ void init() {
   for (i = 0; i < 8; ++i) {
     pinMode(rows[i], OUTPUT);
     pinMode(columns[i], OUTPUT);
-
+    
+    // Clear the display
     digitalWrite(columns[i], HIGH);
   }
 }
